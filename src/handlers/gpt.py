ea65d6084a510a5d53cd4b345da26c3cf54d7dc8
VEGAMETA <viperigor12@gmail.com>
2023-11-20 22:37:14 +0300

    minor fixes

--- src/handlers/gpt.py
+++ src/handlers/gpt.py
@@ -18,11 +18,9 @@ async def gpt_handler(message: aiogram.types.Message) -> None:
 
     if gpt_requests.get(user_id) == GPTRequestStatus.Processing:
         await message.answer('Previous request is processing\nCall /stop to stop answering')
-        logging.info('called while proc')
         return
 
     gpt_requests[user_id] = GPTRequestStatus.Processing
-    logging.info('created proc')
     data = {
         "model": "llama2-uncensored",  # message.from_user.id.get_model()
         "prompt": message.text,
@@ -33,35 +31,33 @@ async def gpt_handler(message: aiogram.types.Message) -> None:
     text = ''
     bot_message_time = time.monotonic()
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post('http://127.0.0.1:11434/api/generate', json=data) as response:
-                async for resp in response.content.iter_chunks():
-                    try:
-                        resp_json = json.loads(resp[0].decode('utf-8'))
-                        text += resp_json.get('response')
-                        if resp_json.get('done'):
-                            await bot_message.edit_text(text)
-                            logging.info(f'{resp_json.get("context")}')
-                            return
-                        if time.monotonic() - bot_message_time > 3:
-                            if gpt_requests.get(user_id) == GPTRequestStatus.StopRequest:
-                                logging.info("called stop req")
-                                await bot_message.edit_text(text + "...")
-                                return
-                            await bot_message.edit_text(text)
-                            bot_message_time = time.monotonic()
-                    except json.decoder.JSONDecodeError as e:
+        async with aiohttp.ClientSession().post('http://127.0.0.1:11434/api/generate', json=data) as response:
+            async for resp in response.content.iter_chunks():
+                try:
+                    if gpt_requests.get(user_id) == GPTRequestStatus.StopRequest:
+                        await bot_message.edit_text(text + "...")
+                        return
+                    resp_json = json.loads(resp[0].decode('utf-8'))
+                    text += resp_json.get('response')
+                    if resp_json.get('done'):
                         await bot_message.edit_text(text)
-                        return logging.error(e)
-                    except aiogram.exceptions.TelegramBadRequest as e:
-                        logging.error(e)
-                        if e.message == "Bad Request: message to edit not found":
-                            return
-                    except aiogram.exceptions.TelegramRetryAfter as e:
-                        logging.error(e)
-                        await asyncio.sleep(e.retry_after)
-    except asyncio.exceptions.TimeoutError as e:
-        await bot_message.edit_text(text + "...\nTimeout Error")
+                        logging.info(f'{resp_json.get("context")}')
+                        return
+                    if time.monotonic() - bot_message_time > 3:
+                        await bot_message.edit_text(text)
+                        bot_message_time = time.monotonic()
+                except json.decoder.JSONDecodeError as e:
+                    await bot_message.edit_text(text)
+                    return logging.error(e)
+                except aiogram.exceptions.TelegramBadRequest as e:
+                    logging.error(e)
+                    if e.message == "Bad Request: message to edit not found":
+                        return
+                except aiogram.exceptions.TelegramRetryAfter as e:
+                    logging.error(e)
+                    await asyncio.sleep(e.retry_after)
+    except asyncio.exceptions.TimeoutError as _:
+        await bot_message.edit_text(text + "...\nTimeout Error (>5min)")
     finally:
         gpt_requests[user_id] = GPTRequestStatus.Stopped
         gpt_requests.pop(user_id)
